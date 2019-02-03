e672771662998ac922721e4ffba0044ce332149a
Marco Sedano <marcoAsedano@gmail.com>
2019-02-02 18:00:51 -0800

    finished up crud todo routes

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-var express = require("express"),
-    app     = express(),
-    port    = 3000;
+var express     = require("express"),
+    app         = express(),
+    port        = 3000,
+    bodyParser  = require("body-parser");
+
+var todoRoutes = require("./routes/todos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.json({message: "Hi from JS object"});
+  res.send("Hello from root route");
 });
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(port, function() {
   console.log("The todoList server has started!");
